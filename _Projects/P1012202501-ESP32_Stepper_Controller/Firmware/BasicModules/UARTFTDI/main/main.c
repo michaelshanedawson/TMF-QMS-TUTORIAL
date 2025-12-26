eeba6d356d39c2bc51728881d5945bc56e32f5d2
michaelshanedawson <michaelshanedawson@gmail.com>
2025-12-26 17:17:28 -0500

    Added new content from stream the other day.

---
+++ _Projects/P1012202501-ESP32_Stepper_Controller/Firmware/BasicModules/UARTFTDI/main/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "../components/uartconsole/uartconsole.h"
+
+uint8_t UART_RX_PIN = 19; // GPIO pin used for the UART RX
+uint8_t UART_TX_PIN = 21; // GPIO pin used for the UART TX
+uint32_t BAUD_RATE = 115200; // Baud rate for UART communication
+char* delimiter = " "; // Delimiter for input parsing
+
+
+void app_main(void)
+{
+    uartconsole_init(UART_RX_PIN, UART_TX_PIN, BAUD_RATE, delimiter);
+    uartconsole_send("\033[H\033[J");
+    //uartconsole_send("Entering main loop.\n");
+    //uartconsole_send("-------------------\n\n");
+    
+    while(1)
+    {
+        ;
+    }
+}
+
+
